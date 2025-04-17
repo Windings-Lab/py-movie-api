fd633f9e9583f13f41a37665f227b328ff8685ff
Windings-Lab <janton20001@gmail.com>
2025-04-17 16:31:42 +0300

    Add movie API and update cinema app
    * Create migration to make movie title unique
    * Add URL routing for movie list and detail endpoints
    * Implement movie views (list, detail, create, update, delete)
    * Update Movie model to ensure unique titles
    * Include cinema app URLs in project-level routing
    * Update .gitignore to ignore __pycache__

--- cinema/models.py
+++ cinema/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
     duration = models.IntegerField(null=True)
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movie_list(request) -> Response | None:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def movie_detail(request, pk: int) -> Response | None:
+    try:
+        movie = get_object_or_404(queryset=Movie.objects.all(), pk=pk)
+    except Http404:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
